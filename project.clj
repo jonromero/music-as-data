b278263777c885703b2d5fa23b3f02c11ad9aab0
Jon Vlachoyiannis <jon@emotionull.com>
2011-03-13 12:26:08 +0200

    lein deps working

--- project.clj
+++ project.clj
@@ -2,8 +2,9 @@
   :description "A Live Programming Language in Clojure using processing"
   :url "http://mad.emotionull.com"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojars.fyuryu/processing.core "1.1.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-					 [swank-clojure "1.2.0"]]
+				 [org.clojars.automata/rosado.processing "1.1.0"]
+				 [org.clojars.automata/ddf.minim "2.1.0"]				 
+				 [org.clojars.automata/minim-spi "2.1.0"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]]
   :jvm-opts ["-Xms256m" "-Xmx1g" "-XX:+UseConcMarkSweepGC" "-server"])
 
