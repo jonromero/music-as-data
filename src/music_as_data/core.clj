fdace9fdbf8239f6c197f42084682d37bd06dea3
Jon Romero Vlachogiannis <darksun4@gmail.com>
2012-09-02 18:09:47 +0300

    many new interesting experiments
    cleaning up code, start using overtone synths

--- src/music_as_data/core.clj
+++ src/music_as_data/core.clj
@@ -32,21 +32,64 @@
 (demo (bpf (play-buf 1 flute-buf (line 0.1 0.3 5)) (line 1 1600)))
 
 
-(definst wobble2 [pitch-freq 440 wobble-freq 5 wobble-depth 5]
-  (let [wobbler (* wobble-depth (sin-osc wobble-freq))
-        freq (+ pitch-freq wobbler)]
-    (sin-osc freq)))
 
 (demo (wobble2 440 1 10))
 (stop)
 
 
-(definst dubstep [freq 100 wobble-freq 2]
+(definst dubstep [freq 100 wobble-freq 2 bla 0]
   (let [sweep (lin-exp (lf-saw wobble-freq) -1 1 40 5000)
         son   (mix (saw (* freq [0.99 1 1.01])))]
     (lpf son sweep)))
 
-(dubstep 440 2)
-(ctl dubstep :wobble-freq 2)
+(dubstep (midi->hz (note :a4)))
+(ctl dubstep :freq (midi->hz (note :b4)))
+
+(def kick (sample (freesound-path 2086))) ;; downloading sounds //wicked!
+(kick)
 
 (stop)
+
+
+(defn defsample2 [n-sym path]
+  (intern *ns* n-sym
+          {:synth wobble2
+           :vol 0.2
+           :pitch pitch
+           :dur 0.1}))
+
+
+(defsample2 iknow "/Users/jonromero/Sources/beat-me-up-scotty/samples/safe_tribble.wav")
+
+(definst wobblez [pitch-freq 440 wobble-freq 5 wobble-depth 100]
+  (let [wobbler (* wobble-depth (sin-osc wobble-freq))
+        freq (+ pitch-freq wobbler)]
+    (sin-osc freq)))
+
+(wobble2)
+
+(intern *ns* (symbol "helloz2")
+        {:synth kick
+         :vol 1.0
+         :pitch 1
+         :dur 0.1})
+
+
+;; first lets load a sound file
+;; create a M.A.D specificication is super easy.
+;; All we need is a synth to feed to overtone
+;; this new structure to be added to the namespace
+
+(defn sword [n-sym your-inst pitch vol dur]
+  (intern *ns* (symbol n-sym)
+          {:synth your-inst
+           :vol vol
+           :pitch pitch
+           :dur dur}))
+
+
+(sword "the-kick" kick 1 1 1)
+(sword "the-kick2" kick 1 1 1)
+(sword "wob" dubstep 440 10 10)
+
+(p (pattern [(assoc wob :pitch 500)]))
